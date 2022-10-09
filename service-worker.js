f9b2ff05d4e7e3bc7ee87e7cd3e83ae83a4e1c94
Wilkor <wilkorguitarrista@msn.com>
2022-10-08 22:46:18 -0300

    fix
    ajustando o top para 7

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.97664f41e490694d77d6512720b30919.js"
+  "./precache-manifest.13f02b2590211c13a7c6f30cba6b0ba0.js"
 );
 
 self.addEventListener('message', (event) => {
