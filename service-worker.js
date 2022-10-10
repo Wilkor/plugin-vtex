b970842932232fb7986044af989e1dd2eff8cbd5
Wilkor <wilkorguitarrista@msn.com>
2022-10-10 18:04:12 -0300

    melhorias
    Adicionando novos elementos do DS da take

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7ee3d7bd9d6a67d40d86b04313a65e91.js"
+  "./precache-manifest.d7658ad0f73d705c9fe8bc7b6fb85ad1.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ static/js/2.92c8aac2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpblip-plugin-example"]=this["webpackJsonpblip-plugin-example"]||[]).push([[2],{33:function(p,i){}}]);
+//# sourceMappingURL=2.92c8aac2.chunk.js.map
